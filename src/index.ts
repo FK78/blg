390730b8b0b5bc987d2e702c4ad156f548fd6611
FK78 <34196381+FK78@users.noreply.github.com>
2026-07-08 17:54:36 +0100

    chore(docker): containerize application with Docker Compose

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { pool } from "./db/db.ts";
 import postsRouter from "./routes/postsRouter.ts";
 
 const app = express();
-const port = process.env.PORT ?? "3000";
+const port = process.env.BLG_PORT ?? "3000";
 
 app.use(express.json());
 
